25fbf4a1caa5b4f6826754c284f5de8dc07ee013
Chris Lemke <spcbrn1@gmail.com>
2017-09-30 18:16:16 -0600

    finished snail

--- kyu_4/Snail.js
+++ kyu_4/Snail.js
@@ -4,35 +4,51 @@
 
 
 let array = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
 ];
 
 let array2 = [
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,8,7,6],
-    [5,4,3,2]
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 8, 7, 6],
+    [5, 4, 3, 2]
 ];
 
 let snail = function(a) {
-    let final = [];
-    let tickRow = 0;
-    let tickCol = 0;
-    while (final.length < Math.pow(a[0].length, 2)) {
-        if (final.length < a.length) {
-            final.push(a[0][1]);
-            tickRow++;
+    let output = [];
+    let tick = 0;
+    let depth = a.length;
+    while (tick < Math.ceil(a.length/2)) {
+        for (i = tick; i < depth; i++) {
+            if (i === tick) {
+                for (j = tick; j < a[i].length - tick; j++) {
+                    output.push(a[i][j]); 
+                }
+            }
+            if (i > tick) {
+                output.push(a[i][depth-1])
+            }
         }
+        for (l = depth - 1; l > tick; l--) {
+            if (l === depth - 1) {
+                for (m = a[l].length-(2+tick); m >= tick; m--) {
+                    output.push(a[l][m]);
+                }
+            }
+            if (l < depth - 1) {
+                output.push(a[l][tick]);
+            }
+        }
+        tick++;
+        depth--;
     }
-    console.log(final)
-    return final;
+    return output;
 }
 
 
 
 
-
-  console.log(snail(array), [1,2,3,6,9,8,7,4,5])
-  console.log(snail(array2), [1,2,3,4,8,6,2,3,4,5,9,5,6,7,7,8])
+console.log(snail(array), [1, 2, 3, 6, 9, 8, 7, 4, 5])
+console.log(snail(array2), [1, 2, 3, 4, 8, 6, 2, 3, 4, 5, 9, 5, 6, 7, 7, 8])
