db0fd4f24e9514b216d1421b829a4ce6d1795a22
Chris Lemke <spcbrn1@gmail.com>
2017-10-27 19:53:45 -0600

    progress

--- javascript/authored/bit-vector-sorting/BitVectorSorting.js
+++ javascript/authored/bit-vector-sorting/BitVectorSorting.js
@@ -20,13 +20,13 @@ const setMaker = () => {
         }
     }
     shuffle(set)
-    return set.join('\n')
+    return set.join('|')
 }
 
 
 
-const input1 = '3\n14\n5\n19\n18\n1\n8\n11\n2';
-const input2 = '19\n5\n7\n8\n14\n17\n11\n13\n1';
+// const input1 = '3\n14\n5\n19\n18\n1\n8\n11\n2';
+// const input2 = '19\n5\n7\n8\n14\n17\n11\n13\n1';
 const input3 = setMaker();
 // const input4 = setMaker();
 // const input5 = setMaker();
@@ -49,11 +49,13 @@ const bitVectorSort = (set) => {
             output.push(i)
         }
     }
-    return output.join('\n')
+    return output.join('|')
 }
 
-console.log(bitVectorSort(input1))
-console.log(bitVectorSort(input2))
-console.log(bitVectorSort(input3))
+
+// console.log(bitVectorSort(input1))
+// console.log(bitVectorSort(input2))
 // console.log(bitVectorSort(input4))
 // console.log(bitVectorSort(input5))
+
+console.log(input3)
--- javascript/authored/bit-vector-sorting/input-generator.js
+++ javascript/authored/bit-vector-sorting/input-generator.js
@@ -1,11 +1,28 @@
+const shuffle = (a) => {
+    var i = 0
+      , j = 0
+      , temp = null
+  
+    for (i = a.length - 1; i > 0; i -= 1) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = a[i]
+      a[i] = a[j]
+      a[j] = temp
+    }
+}
+
 
 const setMaker = () => {
     let set = [];
-    for (i=0; i<9999999; i++) {
+    for (i=0; i<100; i++) {
         let rand = Math.floor(Math.random() * (9 - 1) + 1)
         if (rand > 4) {
             set.push(i)
         }
     }
-    return set.join('\n')
+    shuffle(set)
+    return set.join('|')
 }
+
+
+console.log(setMaker())
