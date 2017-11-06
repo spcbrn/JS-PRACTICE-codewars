ac2e2f5e03db3c90043e1460d065fe0084367b09
Chris Lemke <spcbrn1@gmail.com>
2017-11-06 14:07:28 -0700

    prep for push

---
+++ test.js
@@ -0,0 +1,16 @@
+
+
+const range = (a,b) => {
+    let output = [];
+    for (let i = a; i <= b; i++) {
+        output.push(i);
+    }
+    return output;
+}
+
+
+
+var mult=r=>r.reduce((a,b)=>a*b)
+    , sum=r=>r.reduce((a,b)=>a+b)
+    , sub=r=>r.reduce((a,b)=>a-b)
+    , div=r=>r.reduce((a,b)=>a/b)
