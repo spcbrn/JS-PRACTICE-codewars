2d6a96f4523844213ad9b4fd713c3eed4cb69665
Chris Lemke <spcbrn1@gmail.com>
2017-11-30 14:50:49 -0700

    recursion

--- javascript/kyu_5/PaginationHelper.js
+++ javascript/kyu_5/PaginationHelper.js
@@ -3,15 +3,32 @@
 //kyu: 6
 
 
-const PaginationHelper = (collection, itemsPerPage) => {
+function PaginationHelper(collection, itemsPerPage) {
   this.collection = collection;
   this.perPage = itemsPerPage;
+  this.totalPages = Math.ceil(this.collection.length / this.perPage);
 }
 
-let test = new PaginationHelper([1,2], 1);
+let test = new PaginationHelper(['a','b','c','d','e','f','a','b','c','d','e','f'], 4);
 
-PaginationHelper.prototype.itemCount = () => this.collection.length;
-PaginationHelper.prototype.pageCount = () => this.perPage;
+PaginationHelper.prototype.itemCount = function() {
+  return this.collection;
+};
+PaginationHelper.prototype.pageCount = function() {
+  return this.totalPages
+};
+PaginationHelper.prototype.pageItemCount = function(pageIndex) {
+  let paginatedItems = 0;
+  for (let i = 0; i <= pageIndex; i++) {
+    paginatedItems += this.perPage
+  }
+  let remainder = this.collection.length - paginatedItems;
+  console.log(this.collection.length, paginatedItems, remainder)
+  return remainder < -this.perPage ? -1
+         : null
+};
+PaginationHelper.prototype.pageIndex = function(itemIndex) {
 
+};
 
-console.log(Math.ceil(6/4))
+console.log(test.pageItemCount(3))
---
+++ javascript/kyu_8/CollatzConjecture.js
@@ -0,0 +1,25 @@
+
+//URL: https://www.codewars.com/kata/collatz-conjecture-3n-plus-1/javascript
+//kyu: 8
+
+
+const hotpo = (n, c) => {
+  let count = c ? c : 0;
+  if (n === 0) return 0;
+  if (n === 1) return count;
+  if (n % 2) {
+    count++;
+    return hotpo((3*n + 1), count);
+  } else {
+    count++
+    return hotpo((n/2), count);
+  }
+}
+
+const hotpo = (n, c=0) => n <= 1 ? c : hotpo(n % 2 ? 3*n+1 : n/2, c++)
+
+
+console.log(hotpo(1), 0);
+console.log(hotpo(5), 5);
+console.log(hotpo(6), 8);
+console.log(hotpo(23), 15);
